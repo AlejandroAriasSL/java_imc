217f52dd6c52da5dce1b579d1018bbafdf9a5319
AlejandroAriasSL <alejandroasilva02@gmail>
2025-02-10 14:13:33 +0100

    refactor tests with beforeEach function

--- src/test/java/dev/alejandro/PersonaTest.java
+++ src/test/java/dev/alejandro/PersonaTest.java
@@ -1,5 +1,6 @@
 package dev.alejandro;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -7,15 +8,20 @@ import static org.hamcrest.Matchers.is;
 
 public class PersonaTest {
 
+    private Persona persona;
+    private double kg = 67.5;
+    private double meters = 1.75;
+
+    @BeforeEach
+    void beforeEach(){
+        
+        persona = new Persona(kg, meters);
+    
+    }
     @Test
     @DisplayName("It should return the kg of the person")
     void test_returns_kg_of_person(){
-
-        double kg = 67.7;
-        double meters = 1.75;
-
-        Persona persona = new Persona(kg, meters);
-        
+       
         assertThat(persona.getKg(), is(kg));
 
     }
