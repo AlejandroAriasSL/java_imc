91c61636307c311d7a638b68eef392dd4adfaa57
AlejandroAriasSL <alejandroasilva02@gmail>
2025-02-10 18:19:42 +0100

    added test_prints_normal_weight (tdd:red)

--- src/test/java/dev/alejandro/PersonaTest.java
+++ src/test/java/dev/alejandro/PersonaTest.java
@@ -105,4 +105,24 @@ public class PersonaTest {
 
         System.setOut(oldOut);
     }
+    @Test
+    @DisplayName("It should return 'mild thinness' if IMC is between 17 and 18.5")
+    void test_prints_normal_weight(){
+
+        PrintStream oldOut = System.out;
+
+        double metersThin = 1.80;
+
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(result));
+
+        persona.imcCalc(kg, metersThin);
+
+        String output = result.toString();
+
+        assertThat(output, containsString("Normal weight"));
+
+        System.setOut(oldOut);
+    }
 }
